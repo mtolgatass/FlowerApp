a73c9a239ade8af31921682bcf8a09e06da125ab
Tolga <mtas13@ku.edu.tr>
2019-07-18 21:03:54 +0300

    ready for ui

---
+++ CicekSepetiCase/CicekSepetiCase/FilterHelper.swift
@@ -0,0 +1,22 @@
+//
+//  FilterHelper.swift
+//  CicekSepetiCase
+//
+//  Created by Tolga Taş on 18.07.2019.
+//  Copyright © 2019 Tolga Taş. All rights reserved.
+//
+
+import Foundation
+
+class FilterHelper{
+    
+    static let shared = FilterHelper()
+    
+    func filterData(answer : Welcome) -> [Product]{
+        let filteredByProductGroupId = answer.result.data.products.filter({$0.productGroupID == 1})
+        
+        let filteredByPrice = filteredByProductGroupId.filter({$0.price.current >= 50.0 && $0.price.current <= 100.0})
+        
+        return filteredByPrice
+    }
+}
---
+++ CicekSepetiCase/CicekSepetiCase/ProductsTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ProductsTableViewCell.swift
+//  CicekSepetiCase
+//
+//  Created by Tolga Taş on 18.07.2019.
+//  Copyright © 2019 Tolga Taş. All rights reserved.
+//
+
+import UIKit
+
+class ProductsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
